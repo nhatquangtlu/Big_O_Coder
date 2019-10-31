6fe797ac3390644417da8822710ef03e1a4aa7ca
nhatquangtlu <bquang291@gmail.com>
2019-10-31 07:03:27 -0700

    chia het cho 3 hoac 5

--- NMLT_CHIA_HET_3HOAC_5/main.cpp
+++ NMLT_CHIA_HET_3HOAC_5/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include<cmath>
 using namespace std;
 int main()
 {
@@ -7,6 +6,7 @@ int main()
     cin>>L>>R;
     for(int i=1; i<=R;i++)
         if(i%3==0 || i%5==0)
-            dem++;
+            continue;
+             dem++;
     cout<<""<<dem;
 }
